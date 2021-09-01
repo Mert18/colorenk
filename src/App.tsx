b4da090e0b691b9d447c2b7db09b38cac78507c0
mert18 <mertuygur02@gmail.com>
2021-09-02 00:19:40 +0300

    omit

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,18 @@
-import React, { useState, useEffect } from "react";
 import "./App.css";
 
 const App = () => {
-  const [hue, setHue] = useState(262);
-  const [saturation, setSaturation] = useState(100);
-  const [lightness, setLightness] = useState(50);
-
-  const generate = () => {
-    setHue(Math.trunc(Math.random() * 360));
-  };
-
-  const handleChange = () => {
-    generate();
-  };
-
-  useEffect(() => {
-    generate();
-  }, []);
-
   return (
-    <div
-      style={{
-        backgroundColor: `hsl(${hue}, ${saturation}%, ${lightness}%)`,
-        height: "100vh",
-      }}
-    >
-      <div>
-        <div
-          style={{
-            backgroundColor: `hsl(${hue}, ${saturation - 10}%, ${lightness}%)`,
-            width: "2rem",
-            height: "2rem",
-          }}
-        ></div>
-        <div
-          style={{
-            backgroundColor: `hsl(${hue}, ${saturation - 30}%, ${lightness}%)`,
-            width: "2rem",
-            height: "2rem",
-          }}
-        ></div>
-        <div
-          style={{
-            backgroundColor: `hsl(${hue}, ${saturation - 50}%, ${lightness}%)`,
-            width: "2rem",
-            height: "2rem",
-          }}
-        ></div>
-        <div
-          style={{
-            backgroundColor: `hsl(${hue}, ${saturation - 80}%, ${lightness}%)`,
-            width: "2rem",
-            height: "2rem",
-          }}
-        ></div>
-      </div>
+    <div className="app">
+      <header className="header">
+        <div className="header-hero">
+          <h1>Colorenk</h1>
+          <p>Create beautiful color schemes.</p>
+        </div>
+      </header>
 
-      <div>
-        <div
-          style={{
-            backgroundColor: `hsl(${hue}, ${saturation}%, ${lightness - 10}%)`,
-            width: "2rem",
-            height: "2rem",
-          }}
-        ></div>
-        <div
-          style={{
-            backgroundColor: `hsl(${hue}, ${saturation}%, ${lightness + 10}%)`,
-            width: "2rem",
-            height: "2rem",
-          }}
-        ></div>
-        <div
-          style={{
-            backgroundColor: `hsl(${hue}, ${saturation}%, ${lightness - 20}%)`,
-            width: "2rem",
-            height: "2rem",
-          }}
-        ></div>
-        <div
-          style={{
-            backgroundColor: `hsl(${hue}, ${saturation}%, ${lightness + 20}%)`,
-            width: "2rem",
-            height: "2rem",
-          }}
-        ></div>
-      </div>
+      <main className="main"></main>
 
-      <button onClick={handleChange}>Hello There!</button>
+      <footer className="footer"></footer>
     </div>
   );
 };
